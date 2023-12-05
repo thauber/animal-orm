ac1da1c69abf3273ad6fdeb6b5ee8aceee20ef9a
Tony Hauber <thauber@gmail.com>
2023-12-05 14:34:32 -0600

    fix: explicit retrun values

--- src/a.ts
+++ src/a.ts
@@ -1,18 +1,18 @@
 import { Expr } from 'faunadb';
 import z from 'zod';
 
-function dateTimestamp () {
+function dateTimestamp ():z.ZodPipeline<z.ZodUnion<[z.ZodDate, z.ZodNumber]>, z.ZodNumber> {
   return z.union([z.date(), z.number()]).pipe(z.coerce.number());
 }
-dateTimestamp.optional = function () {
+dateTimestamp.optional = function ():z.ZodPipeline<z.ZodOptional<z.ZodUnion<[z.ZodDate, z.ZodNumber]>>, z.ZodOptional<z.ZodNumber>> {
   return z.union([z.date(), z.number()]).optional().pipe(z.coerce.number().optional());
 }
 
-function timestampDate () {
+function timestampDate ():z.ZodPipeline<z.ZodNumber, z.ZodDate> {
   return z.number().pipe(z.coerce.date());
 }
 
-timestampDate.optional = function () {
+timestampDate.optional = function ():z.ZodPipeline<z.ZodOptional<z.ZodNumber>, z.ZodOptional<z.ZodDate>>  {
   return z.number().optional().pipe(z.coerce.date().optional());
 }
 
