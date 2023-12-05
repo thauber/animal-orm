c71fcb3b91eb1248f527b3611a3b28c74d212c08
Tony Hauber <thauber@gmail.com>
2023-12-05 14:13:56 -0600

    feat: adding date to a

--- src/a.ts
+++ src/a.ts
@@ -1,6 +1,21 @@
 import { Expr } from 'faunadb';
 import z from 'zod';
 
+function dateTimestamp () {
+  return z.union([z.date(), z.number()]).pipe(z.coerce.number());
+}
+dateTimestamp.optional = function () {
+  return z.union([z.date(), z.number()]).optional().pipe(z.coerce.number().optional());
+}
+
+function timestampDate () {
+  return z.number().pipe(z.coerce.date());
+}
+
+timestampDate.optional = function () {
+  return z.number().optional().pipe(z.coerce.date().optional());
+}
+
 export const animal = {
   hidden():z.ZodEffects<z.ZodAny, void, any> {
     return z.any().transform<void>((_a:any)=>{})
@@ -11,7 +26,9 @@ export const animal = {
       message: "Must be a FaunaDB Expr instance",
     })
   },
-  ...z
+  dateTimestamp,
+  timestampDate,
 }
+
 
 export default animal
